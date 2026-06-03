90f1817a4db71f52ead4ab3ed69954eeaf1aae2b
Muhammed Mahmood <muhammedmahmood999@gmail.com>
2026-06-03 13:47:27 +0100

    use skim for LaTeX

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -6,7 +6,6 @@ return {
     vim.g.vimtex_quickfix_open_on_warning = 1
     vim.g.vimtex_quickfix_mode = 2
 
-    -- Let VimTeX manage the 'out' directory natively
     vim.g.vimtex_compiler_latexmk = {
       out_dir = 'out',
       continuous = 0,
@@ -17,16 +16,21 @@ return {
         '-file-line-error',
         '-synctex=1',
         '-interaction=nonstopmode',
-        -- Notice there is NO -outdir flag here. 
-        -- VimTeX injects it automatically based on build_dir!
       },
     }
 
-    vim.g.vimtex_view_method = 'general'
+    -- 2. Tell VimTeX to route RPC SyncTeX calls to Skim
+    vim.g.vimtex_view_method = 'skim'
   end,
   config = function()
+    -- 3. Change this from Single Shot (SS) to the continuous daemon toggle
     vim.keymap.set('n', '<leader>lc', '<cmd>VimtexCompileSS<CR>', { desc = '[L]aTeX [C]ompile (Single Shot)' })
+
+    -- 4. Add a keymap for Forward Search (jump PDF to cursor)
+    vim.keymap.set('n', '<leader>lv', '<cmd>VimtexView<CR>', { desc = '[L]aTeX [V]iew (Forward Search)' })
+
     vim.keymap.set('n', '<leader>lo', '<cmd>VimtexCompileOutput<CR>', { desc = '[L]aTeX [O]utput (Raw Log)' })
     vim.keymap.set('n', '<leader>le', '<cmd>VimtexErrors<CR>', { desc = '[L]aTeX [E]rrors' })
   end,
 }
+
